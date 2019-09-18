b1f66767c23feeeed18c24d4afeca24f1efb6450
Muhammad Adeel <rana.adeel7273@gmail.com>
2019-09-18 23:17:05 +0500

    Working with getters and setters
    Working with getters and setters

--- PracticeJavaIntemediateLibrary/src/main/java/com/example/practicejavaintemediatelibrary/Car.java
+++ PracticeJavaIntemediateLibrary/src/main/java/com/example/practicejavaintemediatelibrary/Car.java
@@ -2,10 +2,20 @@ package com.example.practicejavaintemediatelibrary;
 
 public class Car {
     public String color = "Black";
+    private String interior;
     Car(){
-
+        interior = "Plastic";
     }
-    Car(String selectedColor){
+    Car(String selectedColor, String choosenInerior){
         color = selectedColor;
+        interior = choosenInerior;
+    }
+
+    public String getInterior() {
+        return interior;
+    }
+
+    public void setInterior(String interior) {
+        this.interior = interior;
     }
 }
--- PracticeJavaIntemediateLibrary/src/main/java/com/example/practicejavaintemediatelibrary/CarSimulator.java
+++ PracticeJavaIntemediateLibrary/src/main/java/com/example/practicejavaintemediatelibrary/CarSimulator.java
@@ -4,8 +4,11 @@ public class CarSimulator {
     public static void main(String[] args) {
         System.out.println("Hey! Welcome to Java Programming World.");
         Car myFirstCar = new Car();
-        System.out.println("My first car's color is " + myFirstCar.color);
-        Car mySecondCar = new Car("Blue");
-        System.out.println("My second car's color is " + mySecondCar.color);
+        System.out.println("My first car's color is " + myFirstCar.color + " and its interior is " + myFirstCar.getInterior());
+        Car mySecondCar = new Car("Blue", "Wood");
+        System.out.println("My second car's color is " + mySecondCar.color + " and its interior is " + mySecondCar.getInterior());
+        Car myThirdCar = new Car("Blue", "Wood");
+        myThirdCar.setInterior("Steel");
+        System.out.println("My second car's color is " + myThirdCar.color + " and its interior is " + myThirdCar.getInterior());
     }
 }

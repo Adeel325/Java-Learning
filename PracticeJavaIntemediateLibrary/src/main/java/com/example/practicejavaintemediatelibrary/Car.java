50426d6500dcc4a73723cfd295a4eacbe4199536
Muhammad Adeel <rana.adeel7273@gmail.com>
2019-09-19 01:32:34 +0500

    Static Variables and functions
    Static Variables and functions

--- PracticeJavaIntemediateLibrary/src/main/java/com/example/practicejavaintemediatelibrary/Car.java
+++ PracticeJavaIntemediateLibrary/src/main/java/com/example/practicejavaintemediatelibrary/Car.java
@@ -3,12 +3,15 @@ package com.example.practicejavaintemediatelibrary;
 public class Car {
     public String color = "Black";
     private String interior;
+    public static int price;
     Car(){
         interior = "Plastic";
+        price = 120;
     }
-    Car(String selectedColor, String choosenInerior){
+    Car(String selectedColor, String choosenInerior, int carPrice){
         color = selectedColor;
         interior = choosenInerior;
+        price = carPrice;
     }
 
     public String getInterior() {
--- PracticeJavaIntemediateLibrary/src/main/java/com/example/practicejavaintemediatelibrary/CarSimulator.java
+++ PracticeJavaIntemediateLibrary/src/main/java/com/example/practicejavaintemediatelibrary/CarSimulator.java
@@ -5,10 +5,18 @@ public class CarSimulator {
         System.out.println("Hey! Welcome to Java Programming World.");
         Car myFirstCar = new Car();
         System.out.println("My first car's color is " + myFirstCar.color + " and its interior is " + myFirstCar.getInterior());
-        Car mySecondCar = new Car("Blue", "Wood");
+        Car mySecondCar = new Car("Blue", "Wood", 100);
         System.out.println("My second car's color is " + mySecondCar.color + " and its interior is " + mySecondCar.getInterior());
-        Car myThirdCar = new Car("Blue", "Wood");
+        Car myThirdCar = new Car("Blue", "Wood", 200);
         myThirdCar.setInterior("Steel");
-        System.out.println("My second car's color is " + myThirdCar.color + " and its interior is " + myThirdCar.getInterior());
+        System.out.println("My second car's color is " + myThirdCar.color + " and its interior is " + myThirdCar.getInterior() + ". Its price is $" + Car.price);
+
+        Car corolla = new Car("White", "Wood", 300);
+        // Static variables are accessed using ClassName Dot Variable Name
+        // always have one copy
+        // That's why Both cars have same price $300.
+        System.out.println("My car's color is " + corolla.color + " and its interior is " + corolla.getInterior() + ". Its price is $" + corolla.price);
+        System.out.println("My second car's color is " + myThirdCar.color + " and its interior is " + myThirdCar.getInterior() + ". Its price is $" + Car.price);
+
     }
 }

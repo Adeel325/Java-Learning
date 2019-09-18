c095ada9725bf595dbed18be13d6b41cfefff516
Muhammad Adeel <rana.adeel7273@gmail.com>
2019-09-18 22:52:35 +0500

    Initial
    Initial

---
+++ PracticeJavaIntemediateLibrary/src/main/java/com/example/practicejavaintemediatelibrary/CarSimulator.java
@@ -0,0 +1,7 @@
+package com.example.practicejavaintemediatelibrary;
+
+public class CarSimulator {
+    public static void main(String[] args) {
+        System.out.println("Hew! Welcome to Java Programming World.");
+    }
+}

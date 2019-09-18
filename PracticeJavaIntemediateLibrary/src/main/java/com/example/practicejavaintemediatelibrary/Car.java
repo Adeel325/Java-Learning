dc6436342eafe8a6b7205afc3b136b5a80032701
Muhammad Adeel <rana.adeel7273@gmail.com>
2019-09-18 23:08:40 +0500

    No argument constructor and argument constructor of new class added
    No argument constructor and argument constructor of new class added

---
+++ PracticeJavaIntemediateLibrary/src/main/java/com/example/practicejavaintemediatelibrary/Car.java
@@ -0,0 +1,11 @@
+package com.example.practicejavaintemediatelibrary;
+
+public class Car {
+    public String color = "Black";
+    Car(){
+
+    }
+    Car(String selectedColor){
+        color = selectedColor;
+    }
+}
--- PracticeJavaIntemediateLibrary/src/main/java/com/example/practicejavaintemediatelibrary/CarSimulator.java
+++ PracticeJavaIntemediateLibrary/src/main/java/com/example/practicejavaintemediatelibrary/CarSimulator.java
@@ -2,6 +2,10 @@ package com.example.practicejavaintemediatelibrary;
 
 public class CarSimulator {
     public static void main(String[] args) {
-        System.out.println("Hew! Welcome to Java Programming World.");
+        System.out.println("Hey! Welcome to Java Programming World.");
+        Car myFirstCar = new Car();
+        System.out.println("My first car's color is " + myFirstCar.color);
+        Car mySecondCar = new Car("Blue");
+        System.out.println("My second car's color is " + mySecondCar.color);
     }
 }
